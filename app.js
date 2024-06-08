6729c45bf4437f8d0da392edf94bdb7c369bef5f
Anwar <anwar.namoune.engi@gmail.com>
2024-06-08 20:26:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+const checkWorkingHours = (req, res, next) => {
+  const now = new Date();
+  const day = now.getDay();
+  const hour = now.getHours();
+
+  if (day >= 1 && day <= 5 && hour >= 9 && hour < 17) {
+    next();
+  } else {
+    res.send('The web application is only available during working hours (Monday to Friday, from 9 to 17).');
+  }
+};
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(checkWorkingHours);
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/services', (req, res) => {
+  res.render('services');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
